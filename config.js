893f790ecf4e2f11e40771a54fbb6e5885cc60b6
Juan Francisco <contrerasjf.0@hotmail.com>
2021-02-07 09:39:16 -0600

    feature(Cache): Implement the basic config for the cache layer
    * The Cahche directory for the microservice was defined with the network and index file.
    * New keys were added in the config file
    
    Resolve: C-25

--- config.js
+++ config.js
@@ -17,5 +17,10 @@ module.exports = {
     mysqlService: {
         host: process.env.MYSQL_SRV_HOST || 'localhost',
         port: process.env.MYSQL_SRV_PORT || 3001,
+    },
+    cacheService: {
+        host: process.env.REDIS_HOST || 'localhost',
+        port: process.env.REDIS_PORT || 3003,
     }
+
 }
